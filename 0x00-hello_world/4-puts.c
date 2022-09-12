433d0cf639bfd42d42afc2250af068aec98b5e19
Emmanuel Edhedhe <emmanuel.edhedhe5@gmail.com>
2022-09-12 02:53:09 -0700

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Programming is like building a multilingual puzzle
+ *
+ * Description: Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
